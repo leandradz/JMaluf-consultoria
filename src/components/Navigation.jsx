b4f0df1b395e13b28bc2f0fb36f97e69ed3647c4
Leticia de Andrade <leticia_andrade94@hotmail.com>
2025-12-28 18:46:27 -0300

    feat: implements home page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,7 @@
-import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import './Navigation.css';
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import "./Navigation.css";
+import logo from "../../public/logo.png";
 
 function Navigation() {
   const { t, i18n } = useTranslation();
@@ -11,30 +12,36 @@ function Navigation() {
 
   return (
     <nav className="navigation">
-      <div className="nav-links">
-        <Link to="/">{t('nav.home')}</Link>
-        <Link to="/about">{t('nav.about')}</Link>
-        <Link to="/contact">{t('nav.contact')}</Link>
+      <div className="nav-logo">
+        <img src={logo} alt="JMaluf Consultoria" width={24} /> JMaluf
+        Consultoria
       </div>
-      <div className="language-switcher">
-        <button 
-          onClick={() => changeLanguage('pt-BR')}
-          className={i18n.language === 'pt-BR' ? 'active' : ''}
-        >
-          PT
-        </button>
-        <button 
-          onClick={() => changeLanguage('en')}
-          className={i18n.language === 'en' ? 'active' : ''}
-        >
-          EN
-        </button>
-        <button 
-          onClick={() => changeLanguage('es')}
-          className={i18n.language === 'es' ? 'active' : ''}
-        >
-          ES
-        </button>
+      <div style={{ display: "flex", gap: 48, alignItems: "center" }}>
+        <div className="nav-links">
+          <Link to="/">{t("nav.home")}</Link>
+          <Link to="/about">{t("nav.about")}</Link>
+          <Link to="/contact">{t("nav.contact")}</Link>
+        </div>
+        <div className="language-switcher">
+          <button
+            onClick={() => changeLanguage("pt-BR")}
+            className={i18n.language === "pt-BR" ? "active" : ""}
+          >
+            PT
+          </button>
+          <button
+            onClick={() => changeLanguage("en")}
+            className={i18n.language === "en" ? "active" : ""}
+          >
+            EN
+          </button>
+          <button
+            onClick={() => changeLanguage("es")}
+            className={i18n.language === "es" ? "active" : ""}
+          >
+            ES
+          </button>
+        </div>
       </div>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
-import { useTranslation } from 'react-i18next';
-import '../styles/pages.css';
+import { useTranslation } from "react-i18next";
+import "../styles/pages.css";
+import logo from "../../public/jmaluf-consultoria.png";
 
 function Home() {
   const { t } = useTranslation();
 
   return (
-    <div className="page">
-      <h1>{t('home.title')}</h1>
-      <p>{t('home.description')}</p>
+    <div
+      className="page"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        // marginTop: 48,
+        gap: 24,
+      }}
+    >
+      <img src={logo} alt="Description" width={508}></img>
+      <div>
+        <h1 style={{ fontSize: 48, fontWeight: 900 }}>{t("home.title")}</h1>
+        <p style={{ fontSize: 16, lineHeight: "24px" }}>
+          {t("home.description")}
+        </p>
+      </div>
     </div>
   );
 }
