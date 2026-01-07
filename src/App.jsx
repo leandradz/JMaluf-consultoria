b27622dc9421a36e1c702352bd2b7cf1bf63c17b
Leticia de Andrade <leticia_andrade94@hotmail.com>
2026-01-07 19:00:46 -0300

    fix: change import

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 import Home from "./pages/Home/Home/Home";
-import About from "./pages/About/About";
-import Contact from "./pages/Contact/Contact";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import "./App.css";
 
 function App() {
