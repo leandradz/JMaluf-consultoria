fa21eee2c49bda8538cbf3ae31503920368f54c1
Leticia de Andrade <leticia_andrade94@hotmail.com>
2026-01-07 18:58:28 -0300

    fix: change route path

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation/Navigation';
-import Home from './pages/Home/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
+    <Router basename={import.meta.env.VITE_PUBLIC_URL || "/"}>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
   );
 }
